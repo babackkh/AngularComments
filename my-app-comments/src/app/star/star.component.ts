651186d730c55af8b4c215bbd13d40cd17887a1e
baback <bob.khademi@gmail.com>
2019-09-04 23:05:42 +0430

    Variable Duplication Debug

--- my-app-comments/src/app/star/star.component.ts
+++ my-app-comments/src/app/star/star.component.ts
@@ -9,23 +9,18 @@ export class StarComponent implements OnInit {
   starWidth: number;
   reviewCount = 0;
   sum = 0;
+  rate = 0;
+  qnty = 0;
+  starWidth2 = 0;
 
-  sum:number = 0;
-  rate:number = 0;
-  qnty:number = 0;
-  _starWidth:number = 0;
-
-  onStarClick(nr){
+  onStarClick(nr: any) {
     // console.log(nr);
     this.qnty++;
     this.sum += nr;
     this.rate = this.sum / this.qnty;
-    // console.log(this.qnty);
-    // console.log(this.sum);
-    // console.log(this.rate);
 
-    this._starWidth = this.rate * 90 / 5;
-    console.log(this._starWidth);
+    this.starWidth2 = this.rate * 90 / 5;
+    console.log(this.starWidth2);
   }
 
   ngOnInit() {
@@ -33,7 +28,7 @@ export class StarComponent implements OnInit {
   starClicked(rate: number) {
     this.reviewCount++;
     this.sum = this.sum += rate;
-    this.starWidth = ( this.sum / this.reviewCount) * 90 / 5;
+    this.starWidth2 = ( this.sum / this.reviewCount) * 90 / 5;
   }
 
 
